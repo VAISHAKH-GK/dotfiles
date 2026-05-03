96b6f8661bdf9884391ff5a1aa45893475f01bd8
Vaishakh GK <vaishakh@vaishakhgk.com>
2026-05-04 00:07:56 +0530

    Fish fix sddm issue

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 if status is-interactive
+
+  if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) > /dev/null
+  end
+
+
   # Commands to run in interactive sessions can go here
-  alias python=python3
-  alias pip=pip3
   alias mv='mv -i'
   alias rm='rm -i'
   alias ls='eza --group-directories-first --icons'
@@ -25,12 +29,12 @@ function fish_user_key_bindings
   fish_vi_key_bindings
 end
 
-set -x ANDROID_SDK_ROOT /opt/android-sdk
+set -x ANDROID_SDK_ROOT ~/Android/Sdk
 set -x ANDROID_HOME $ANDROID_SDK_ROOT
 set -x ANDROID_AVD_HOME $HOME/.android/avd
 set -x PATH $PATH $ANDROID_SDK_ROOT/cmdline-tools/latest/bin $ANDROID_SDK_ROOT/emulator $ANDROID_SDK_ROOT/platform-tools
-set -x JAVA_HOME /usr/lib/jvm/java-21-openjdk
-set -x GRADLE_LOCAL_JAVA_HOME /usr/lib/jvm/java-21-openjdk
+set -x JAVA_HOME /usr/lib/jvm/java-25-openjdk
+set -x GRADLE_LOCAL_JAVA_HOME /usr/lib/jvm/java-25-openjdk
 
 set -x PATH $PATH /home/vaishakh/.local/bin
 set -x PATH $PATH ~/.cargo/bin/
@@ -48,10 +52,10 @@ set -x GOTELEMETRY off
 set -x EDITOR emacsclient
 set -x BROWSER zen-browser
 
-set MOZ_ENABLE_WAYLAND 1
-set -x LD_LIBRARY_PATH /usr/local/lib
-
-set -x XDG_CONFIG_HOME $HOME/.config
+# set -x LD_LIBRARY_PATH /usr/local/lib
 eval (ssh-agent -c) > /dev/null
 
+set -x XDG_CONFIG_HOME $HOME/.config
+
 set -gx NVM_DIR $HOME/.nvm
+fish_vi_key_bindings 2>/dev/null
