5642ede24a292bd26a8e9998c50324fe55d9a3cb
Vaishakh GK <vaishakh@vaishakhgk.com>
2026-05-14 14:18:51 +0530

    Go mod added

--- .config/emacs/editor.el
+++ .config/emacs/editor.el
@@ -1,69 +1,44 @@
 ;; Apply .editorconfig file configurations
 (editorconfig-mode 1)
 
-;; Tab width
 ;; Global defaults
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq-default standard-indent 4)
 
-;; C / C++
+;; Language indentation
 (setq-default c-basic-offset 4)
-
-;; JavaScript
-(setq js-indent-level 4)
-
-;; Python
 (setq python-indent-offset 4)
-
-;; Lua
+(setq js-indent-level 4)
 (setq lua-indent-level 4)
 
-;; Lisp family
+;; Go
+(use-package go-mode
+  :hook
+  (go-mode . (lambda ()
+               (setq-local tab-width 4)
+               (setq-local indent-tabs-mode t))))
+
+;; Lisp indentation
 (setq lisp-body-indent 2)
 
-;; Emacs Lisp
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (setq-local indent-tabs-mode nil)))
-
-;; Common Lisp
-(add-hook 'lisp-mode-hook
-          (lambda ()
-            (setq-local indent-tabs-mode nil)))
-
-;; Scheme
-(add-hook 'scheme-mode-hook
-          (lambda ()
-            (setq-local indent-tabs-mode nil)))
-
-;; Go
-(add-hook 'go-mode-hook
-          (lambda ()
-            ;; Use real tabs
-            (setq-local indent-tabs-mode t)
-
-            ;; Display width of tabs
-            (setq-local tab-width 4)
-
-            ;; Make TAB insert literal tabs
-            (local-set-key (kbd "TAB") #'self-insert-command)))
-
+;; Disable electric indentation
 (electric-indent-mode -1)
+
+;; Autoclose pairs
 (electric-pair-mode 1)
 
-
-;; Automatically reload changed files
+;; Auto reload changed files
 (global-auto-revert-mode 1)
 
-;; ;; Preserve minibuffer history
+;; Save minibuffer history
 (savehist-mode 1)
 
-;; ;; Remember cursor positions
+;; Remember cursor positions
 (save-place-mode 1)
 
 ;; Better scrolling
 (setq scroll-conservatively 101)
 
-;; Short answers
+;; Short prompts
 (fset 'yes-or-no-p 'y-or-n-p)
